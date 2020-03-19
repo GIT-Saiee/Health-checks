a9d294250fbfcd3f6b957df800cb5bc287690280
Saiee <sreesaiee@gmail.com>
2020-03-19 15:36:30 -0700

    Added a comment add check_disk_full function and changed the call to function check_disk_full

--- all_checks.py
+++ all_checks.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 import os
 import sys
-
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
+#need to add a check_disk_full function
 def main():
     if check_reboot():
         print("pending reboot")
         sys.exit(1)
-    if check_disk_full("/", 2, 10):
+    if check_disk_full(disk="/",min-gb= 2, min_percent=10):
         print("Disk full.")
     sys.exit(1)
 
